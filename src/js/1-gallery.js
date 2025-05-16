8e8e121781f72bb288a1db1f35004269f373d8f1
Stanislav Porohnavets <stasporoxnavec@gmail.com>
2025-05-16 17:42:24 +0300

    hw09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -2,6 +2,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+
 const images = [
   { preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg", original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg", description: "Hokkaido Flower" },
   { preview: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg", original: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg", description: "Container Haulage Freight" },
